85573c683d0f110b23e55586107d0dde1fc92450
JoaDoVinis <vinilago515@gmail.com>
2025-05-16 20:41:22 -0300

    Update index.js

--- scr/pages/routes/index.js
+++ scr/pages/routes/index.js
@@ -1,7 +1,8 @@
 import  { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import { Home } from '../home';
-import { Login } from '../login';
-import { Register } from '../register';
+import { Home } from '..pages/home';
+import { Login } from '..pages/login';
+import { Profile} from '..pages/Profile';
+import { cart } from '..pages/cart';
 
 const stack = createNativeStackNavigator();
