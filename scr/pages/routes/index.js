17026c4ef166427a1ba05da1f4e8df10994cd7fc
JoaDoVinis <vinilago515@gmail.com>
2025-05-16 20:39:37 -0300

    Create index.js
    rotas criadas

---
+++ scr/pages/routes/index.js
@@ -0,0 +1,7 @@
+import  { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import { Home } from '../home';
+import { Login } from '../login';
+import { Register } from '../register';
+
+const stack = createNativeStackNavigator();
