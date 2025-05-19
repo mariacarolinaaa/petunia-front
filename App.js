28a3d45e88ea4f76c3381ba7aaee6f82f1a4a4aa
JoaDoVinis <vinilago515@gmail.com>
2025-05-19 18:53:08 -0300

    Update App.js
    concertar erro

--- App.js
+++ App.js
@@ -1,19 +1,16 @@
+import { StatusBar } from 'react-native';
 import React from 'react';
-import { StatusBar } from 'react-native'
-import { StyleSheet, Text, View } from 'react-native';
 
 import { NavigationContainer } from '@react-navigation/native';
-import { Routes } from './scr/pages/routes';
-
-
+import  Routes  from './scr/pages/routes';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>petunia-boards</Text>      
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Routes />
+    </NavigationContainer>
   );
 }
+
 
 
